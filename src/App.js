c54c266a1feb2158e4e9bccf2eb4f6410465699d
samroc <samroc@users.noreply.github.com>
2022-06-09 09:46:32 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGripVertical } from '@fortawesome/free-solid-svg-icons'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
