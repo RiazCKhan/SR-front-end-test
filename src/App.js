08903faab57b9b68aab82efa36cf72198234180e
RiazK <RiazCKhan@gmail.com>
2022-07-04 14:38:37 -0400

    remove unused imports

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import './App.css';
 import './index.css';
 
 import PurchaseList from './components/PurchaseList';
-import PurchaseListItem from './components/PurchaseListItem';
 import ShipmentList from './components/ShipmentList';
-import ShipmentListItem from './components/ShipmentListItem';
 
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGripVertical } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
+
+
+
   return (
     <section className='main'>
       <div className='form-content'>
