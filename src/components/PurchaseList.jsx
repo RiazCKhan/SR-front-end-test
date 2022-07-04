e711a536d928b9853ae606f18de7bbc43e22561f
RiazK <RiazCKhan@gmail.com>
2022-07-04 11:45:00 -0400

    import data and purchase list item -- map through data and render purchaselistitem component

--- src/components/PurchaseList.jsx
+++ src/components/PurchaseList.jsx
@@ -1,5 +1,25 @@
+import PurchaseListItem from "./PurchaseListItem"
+import augShipment from '../data/api-data1'
+
 export default function PurchaseList() {
-  return(
-    <h1>PurchaseList</h1>
+
+  const allOrders = augShipment[0].elements
+
+  const eachOrder = allOrders.map((order) => {
+    return (
+      <PurchaseListItem
+        key={order.id}
+        customer={order.list[0]['name']}
+        purchaseOrderNumber={order.list[1]['name']}
+        shipper={order.list[2]['name']}
+        cases={order.list[3]['name']}
+      />
+    )
+  })
+
+  return (
+    <>
+      {eachOrder}
+    </>
   )
 }
