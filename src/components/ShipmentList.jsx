d4258a4512384803d1d9dfa650d7a7af11538a38
RiazK <RiazCKhan@gmail.com>
2022-07-04 12:30:15 -0400

    map data and render shipmentlistitem with data

--- src/components/ShipmentList.jsx
+++ src/components/ShipmentList.jsx
@@ -1,5 +1,25 @@
+import ShipmentListItem from "./ShipmentListItem";
+import augShipment from "../data/api-data1";
+
 export default function ShipmentList() {
-  return(
-    <h1>ShipmentList</h1>
+
+  const allShipmentOrders = augShipment[1].elements
+
+  const eachShipmentOrder = allShipmentOrders.map((order) => {
+    return (
+      <ShipmentListItem 
+      key={order.id}
+      driver={order.list[0]['name']}
+      Temp={order.list[1]['name']}
+      Truck={order.list[2]['name']}
+      Trailer={order.list[3]['name']}
+      />
+    )
+  })
+
+  return (
+    <>
+    {eachShipmentOrder}
+    </>
   )
 }
