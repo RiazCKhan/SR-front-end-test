06441d5f2e040bbfff921326c4c7a21605e07071
RiazK <RiazCKhan@gmail.com>
2022-07-04 16:50:35 -0400

    Step 1: grab index and corresponding form input

--- src/components/PurchaseList.jsx
+++ src/components/PurchaseList.jsx
@@ -1,18 +1,33 @@
+import { useState } from "react";
 import PurchaseListItem from "./PurchaseListItem";
 import augShipment from '../data/api-data1';
 
 export default function PurchaseList() {
 
-  const allPurchaseOrders = augShipment[0].elements
+  const [customerName, setCustomerName] = useState([])
 
-  const eachPurchaseOrder = allPurchaseOrders.map((order) => {
+  const updateHandler = (index, data) => {
+    setCustomerName((customers) => customers.map((customer, i) => {
+      if (index === i) {
+        return { ...customer, ...data };
+      }
+      return customer;
+    }));
+  }
+
+  const allPurchaseOrders = augShipment[0].elements
+  const eachPurchaseOrder = allPurchaseOrders.map((order, index) => {
     return (
       <PurchaseListItem
         key={order.id}
+        {...order}
+        index={index}
         customer={order.list[0]['name']}
         purchaseOrderNumber={order.list[1]['name']}
         shipper={order.list[2]['name']}
         cases={order.list[3]['name']}
+        customerName={customerName}
+        updateHandler={updateHandler}
       />
     )
   })
--- src/components/PurchaseListItem.jsx
+++ src/components/PurchaseListItem.jsx
@@ -1,29 +1,51 @@
+import { jediOrder } from 'fontawesome'
 import './PurchaseListItem.css'
 
 export default function PurchaseListItem(props) {
 
-const { customer, purchaseOrderNumber, shipper, cases } = props
+  const { customer, purchaseOrderNumber, shipper, cases, index, customerName, updateHandler } = props
 
   return (
     <section className='purchase-card'>
       <article>
         <div>{customer}</div>
-        <input placeholder="String" />
+        <input
+          type="text"
+          name="customerName"
+          id="customerName"
+          value={customerName[index]}
+          placeholder="String"
+          onChange={(event) => updateHandler(index, { customerName: event.target.value })} />
       </article>
 
       <article>
         <div>{purchaseOrderNumber}</div>
-        <input placeholder="String" />
+        <input
+          typeof='text'
+          name="purchase-order-number"
+          // value={purchaseOrderNumberInfo}
+          placeholder="String"
+          onChange={(event) => { }} />
       </article>
 
       <article>
         <div>{shipper}</div>
-        <input placeholder="String" />
+        <input
+          type="text"
+          name="shipper"
+          // value={purchaseOrderShipperInfo}
+          placeholder="String"
+          onChange={(event) => { }} />
       </article>
 
       <article>
         <div>{cases}</div>
-        <input placeholder="Number" />
+        <input
+          type="text"
+          name="cases"
+          // value={purchaseOrderCasesInfo}
+          placeholder="Number"
+          onChange={(event) => { }} />
       </article>
     </section>
   )
