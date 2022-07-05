2157b2c6b3189a66a9ca2b7e8dbead3bc720c2a8
RiazK <RiazCKhan@gmail.com>
2022-07-05 11:13:37 -0400

    inital commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import PurchaseList from "./PurchaseList"
+import ShipmentList from "./ShipmentList"
+
+export default function Main() {
+  return (
+    <>
+    <PurchaseList />
+    <ShipmentList />
+    </>
+  )
+}
