670471b0315d262209b9c24cc4b3471d2b51e807
RiazK <RiazCKhan@gmail.com>
2022-07-05 11:26:38 -0400

    pass state to child

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react"
 import PurchaseList from "./PurchaseList"
 import ShipmentList from "./ShipmentList"
 
 export default function Main() {
+
+  const [purchaseData, setPurchaseData] = useState([])
+  const [shipmentData, setShipmentData] = useState([])
+
   return (
     <>
-    <PurchaseList />
-    <ShipmentList />
+      <PurchaseList
+        setPurchaseData={setPurchaseData}
+      />
+      <ShipmentList />
     </>
   )
 }
