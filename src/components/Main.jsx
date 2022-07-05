f2acdfb228935d8d5f7afc79f032a3bb003c53cf
RiazK <RiazCKhan@gmail.com>
2022-07-05 11:48:41 -0400

    create submit and fakesubmit FNs

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,22 +1,41 @@
+import axios from "axios"
 import { useState } from "react"
 import PurchaseList from "./PurchaseList"
 import ShipmentList from "./ShipmentList"
+
+import './Main.css'
 
 export default function Main() {
 
   const [purchaseData, setPurchaseData] = useState([])
   const [shipmentData, setShipmentData] = useState([])
 
-  console.log(shipmentData)
+  const fakeSubmit = () => {
+    console.log('Yah you did it!')
+  }
+
+  const submitForm = () => {
+    const formData = {
+      purchaseData,
+      shipmentData
+    }
+
+    axios.post('/pathway', { formData })
+      .then(res => console.log('Sending Data: ', formData))
+      .catch(error => console.log('Oops... Something went wrong', error))
+  }
 
   return (
     <>
       <PurchaseList
         setPurchaseData={setPurchaseData}
       />
-      <ShipmentList 
-      setShipmentData={setShipmentData}
+      <ShipmentList
+        setShipmentData={setShipmentData}
       />
+      <div>
+        <button className="submit-btn" onClick={() => fakeSubmit()}>Submit</button>
+      </div>
     </>
   )
 }
