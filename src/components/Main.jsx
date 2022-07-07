d121470c86b219f12a01ed2ade5d904222c82552
RiazK <RiazCKhan@gmail.com>
2022-07-07 10:31:21 -0400

    Move State, Move Beautiful DnD feature, Move Render of PurchaseList Map, Create onChangeHandler

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,41 +1,129 @@
-import axios from "axios"
-import { useState } from "react"
+import { useState, useEffect, useRef, createRef, forwardRef } from "react"
 import PurchaseList from "./PurchaseList"
 import ShipmentList from "./ShipmentList"
+import axios from "axios"
+import augShipment from '../data/api-data1';
+
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGripVertical } from '@fortawesome/free-solid-svg-icons'
+
+
+import { updateNameHandler, updatePurchaseHandler, updateShipperHandler, updateCaseHandler, aggregateCustomerShipData } from "../helpers/functions";
 
 import './Main.css'
 
 export default function Main() {
+  const [customerName, setCustomerName] = useState({})
+  const allPurchaseOrders = augShipment[0].elements
+  const [customerOrders, setCustomerOrders] = useState(allPurchaseOrders)
 
   const [purchaseData, setPurchaseData] = useState([])
   const [shipmentData, setShipmentData] = useState([])
 
-  const submitForm = (event) => {
 
-    if (Object.keys(purchaseData).length === 0) return false
-    if (Object.keys(shipmentData).length === 0) return false
+  const validatePurchase = () => {
+    // console.log("testing ref",validator.current.state)
+    // console.log(invalid.current = "error")
+    const allPurchaseOrders = augShipment[0].elements
+    console.log("i am validating your purchase")
+    console.log(Object.keys(customerName))
+    console.log(Object.values(customerName))
+    console.log(Object.keys(allPurchaseOrders))
 
-    const formData = {
-      purchaseData,
-      shipmentData
+    let isValid = true
+    const invalids = []
+    for (let key of Object.keys(allPurchaseOrders)) {
+      if (!customerName[key] || customerName[key] === '') {
+        invalids.push(key)
+        isValid = false
+      }
     }
 
-    axios.post('api/form', { formData })
-      .then(res => console.log('Sending Data: ', formData))
-      .catch(error => console.log('Oops... Something went wrong', error))
+    console.log("but is it valid? ", isValid)
+    console.log("the invalids: ", invalids)
+    // console.log('name', purchaseData['0']['name'])
+
   }
 
+  const submitForm = (event) => {
+    validatePurchase()
+
+    // if (validatePurchase() === false) return
+    // if (validateShipment() === false) return 
+
+    // if (Object.keys(purchaseData).length === 0) return false
+    // if (Object.keys(shipmentData).length === 0) return false
+
+    // const formData = {
+    //   purchaseData,
+    //   shipmentData
+    // }
+
+    // axios.post('api/form', { formData })
+    //   .then(res => console.log('Sending Data: ', formData))
+    //   .catch(error => console.log('Oops... Something went wrong', error))
+  }
+
+  const customerOnChange = (event, index) => {
+    setCustomerName(updateNameHandler(index, customerName, event.target.value))
+  }
+
+  const handleChange = (event, id) => {
+    console.log(id)
+    console.log(event.target.value)
+    const text = event.target.value;
+    customerOnChange(event, id);
+  };
+
+  function handleOnDragEnd(result) {
+    console.log(result)
+    const orders = Array.from(customerOrders)
+    const [reorderedItem] = orders.splice(result.source.index, 1);
+    orders.splice(result.destination.index, 0, reorderedItem);
+    setCustomerOrders(orders)
+  }
+
+  const purchaseItems = customerOrders.map((order, index) => (
+    <Droppable droppableId="customerInfo">
+      {(provided) => (
+        <ul {...provided.droppableProps} ref={provided.innerRef}>
+          <Draggable key={order.id} draggableId={order.id} index={index}>
+            {(provided) => (
+              <li {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef} className='purchase-card'> <FontAwesomeIcon icon={faGripVertical} size={"2x"} />
+                <PurchaseList
+                  setPurchaseData={setPurchaseData}
+                  customerName={customerName}
+                  handleChange={handleChange}
+                  index={index}
+                  errorClass={"error"}
+                  customerOrders={customerOrders}
+                  setCustomerOrders={setCustomerOrders}
+                />
+              </li>
+            )}
+          </Draggable>
+          {provided.placeholder}
+        </ul>
+      )}
+    </Droppable>
+  ))
+
+
   return (
-    <form>
-      <PurchaseList
-        setPurchaseData={setPurchaseData}
-      />
-      <ShipmentList
-        setShipmentData={setShipmentData}
-      />
-      <div className="btn-container">
-        <button className="submit-btn" onClick={(event) => { submitForm(event) }}>Submit</button>
-      </div>
-    </form>
+    <>
+      <h3>{augShipment[0]['sectionTitle']}</h3>
+      <form>
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+          {purchaseItems}
+        </DragDropContext>
+        <ShipmentList
+          setShipmentData={setShipmentData}
+        />
+        <div className="btn-container">
+          <button className="submit-btn" onClick={(event) => { submitForm(event) }}>Submit</button>
+        </div>
+      </form>
+    </>
   )
 }
--- src/components/PurchaseList.jsx
+++ src/components/PurchaseList.jsx
@@ -3,106 +3,76 @@ import augShipment from '../data/api-data1';
 
 import { updateNameHandler, updatePurchaseHandler, updateShipperHandler, updateCaseHandler, aggregateCustomerShipData } from "../helpers/functions";
 
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGripVertical } from '@fortawesome/free-solid-svg-icons'
-
 import "./PurchaseList.css";
 
 export default function PurchaseList(props) {
 
-  const { setPurchaseData } = props
+  const { customerName, handleChange, index, errorClass, setPurchaseData, customerOrders, setCustomerOrders } = props
 
-  const [customerName, setCustomerName] = useState({})
   const [purchaseOrder, setPurchaseOrder] = useState({})
   const [shipperName, setShipperName] = useState({})
   const [caseNumber, setCaseNumber] = useState({})
+
 
   // useEffect Updating Parent State
   useEffect(() => {
     setPurchaseData(aggregateCustomerShipData(customerName, purchaseOrder, shipperName, caseNumber))
   }, [setPurchaseData, customerName, purchaseOrder, shipperName, caseNumber])
 
-  const allPurchaseOrders = augShipment[0].elements
-  const [customerOrders, setCustomerOrders] = useState(allPurchaseOrders)
 
-  const handleOnDragEnd = (result) => {
-    const orders = Array.from(customerOrders)
-    const [reorderedItem] = orders.splice(result.source.index, 1);
-    orders.splice(result.destination.index, 0, reorderedItem);
-
-    setCustomerOrders(orders)
-  }
+  const order = customerOrders[index]
 
   return (
     <>
-      <h3>{augShipment[0]['sectionTitle']}</h3>
-      <DragDropContext onDragEnd={handleOnDragEnd}>
-        <Droppable droppableId="customerInfo">
-          {(provided) => (
-            <ul {...provided.droppableProps} ref={provided.innerRef}>
-              {customerOrders.map((order, index) => {
-                return (
-                  <Draggable key={order.id} draggableId={order.id} index={index}>
-                    {(provided) => (
-                      <li {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef} className='purchase-card'> <FontAwesomeIcon icon={faGripVertical} size={"2x"} />
-                        <article>
-                          <div>{order.list[0]['name']}</div>
-                          <input
-                            type="text"
-                            name="customer-name"
-                            value={customerName[index] || ""}
-                            placeholder="String"
-                            required
-                            onChange={(event) => setCustomerName(updateNameHandler(index, customerName, event.target.value))}
-                          />
-                        </article>
+      <article>
+        <div>{order.list[0]['name']}</div>
+        <input
+          className={errorClass}
+          type="text"
+          name="customer-name"
+          value={customerName[index] || ""}
+          placeholder="String"
+          required
+          onChange={(event) => { handleChange(event, index) }}
+        />
+      </article>
 
-                        <article>
-                          <div>{order.list[1]['name']}</div>
-                          <input
-                            typeof='text'
-                            name="purchase-order-number"
-                            value={purchaseOrder[index] || ""}
-                            placeholder="String"
-                            required
-                            onChange={(event) => setPurchaseOrder(updatePurchaseHandler(index, purchaseOrder, event.target.value))}
-                          />
-                        </article>
+      <article>
+        <div>{order.list[1]['name']}</div>
+        <input
+          typeof='text'
+          name="purchase-order-number"
+          value={purchaseOrder[index] || ""}
+          placeholder="String"
+          required
+          onChange={(event) => setPurchaseOrder(updatePurchaseHandler(index, purchaseOrder, event.target.value))}
+        />
+      </article>
 
-                        <article>
-                          <div>{order.list[2]['name']}</div>
-                          <input
-                            type="text"
-                            name="shipper"
-                            value={shipperName[index] || ""}
-                            placeholder="String"
-                            required
-                            onChange={(event) => setShipperName(updateShipperHandler(index, shipperName, event.target.value))}
-                          />
-                        </article>
+      <article>
+        <div>{order.list[2]['name']}</div>
+        <input
+          type="text"
+          name="shipper"
+          value={shipperName[index] || ""}
+          placeholder="String"
+          required
+          onChange={(event) => setShipperName(updateShipperHandler(index, shipperName, event.target.value))}
+        />
+      </article>
 
-                        <article>
-                          <div>{order.list[3]['name']}</div>
-                          <input
-                            type="text"
-                            name="cases"
-                            value={caseNumber[index] || ""}
-                            placeholder="Number"
-                            required
-                            onChange={(event) => setCaseNumber(updateCaseHandler(index, caseNumber, event.target.value))}
-                          />
-                        </article>
-                      </li>
-                    )}
-                  </Draggable>
-                )
-              })}
-              {provided.placeholder}
-            </ul>
-          )}
-        </Droppable>
-      </DragDropContext>
+      <article>
+        <div>{order.list[3]['name']}</div>
+        <input
+          type="text"
+          name="cases"
+          value={caseNumber[index] || ""}
+          placeholder="Number"
+          required
+          onChange={(event) => setCaseNumber(updateCaseHandler(index, caseNumber, event.target.value))}
+        />
+      </article>
     </>
-  )
+  );
 }
+
